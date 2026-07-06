5704a8dad9915084bf383639f34ac40189cd9d95
zachsanford <sanfordzachary@gmail.com>
2026-07-06 14:54:50 -0700

    rough draft. need to cleanup.

--- full-test.py
+++ full-test.py
@@ -1,7 +1,11 @@
+import argparse
 from modtestfuncs import full_test
 
 def main():
-    full_test()
+    parser = argparse.ArgumentParser(description="Download and map NOAA Weather Alerts.")
+    parser.add_argument("--state", default="OR",help="Two-letter state abbreviation (default: OR)")
+    args = parser.parse_args()
+    full_test(args.state)
 
 if __name__ == '__main__':
     main()
--- modtestfuncs.py
+++ modtestfuncs.py
@@ -7,7 +7,6 @@
 
 import requests
 import geopandas as gpd
-import matplotlib.pyplot as plt
 from noaa_sdk import NOAA
 
 # Y, X point data test
@@ -43,8 +42,65 @@ def raw():
     except:
         print("NO GEOMETRY IN THE API RESPONSE\n\nQUITTING..")
 
-def full_test():
-    core_response = requests.get(r'https://api.weather.gov/alerts/active?area=OR') # area=OR
+def full_test(location):
+    print(f"Gathering Alerts for {location}...")
+    core_response = requests.get(rf'https://api.weather.gov/alerts/active?area={location}') # area=OR
     core_data = core_response.json()
+    print(f"...Found {len(core_data["features"])} alerts.")
+    print(f"Mapping found alerts for {location}...")
 
-    print(core_data)
+    temp_tbl = []
+
+    # Gather zones
+
+    for feature in core_data["features"]:
+        alert_props = feature["properties"]
+        props = {
+            "Alert_id": feature.get("id"),
+            "Event": alert_props.get("event"),
+            "Headline": alert_props.get("headline"),
+            "Description": alert_props.get("description"),
+            "Severity": alert_props.get("severity"),
+            "Urgency": alert_props.get("urgency"),
+            "Certainty": alert_props.get("certainty"),
+            "Effective": alert_props.get("effective"),
+            "Expires": alert_props.get("expires"),
+            "Status": alert_props.get("status"),
+            "Message_type": alert_props.get("messageType"),
+            "Sender": alert_props.get("senderName")
+        }
+
+        if feature["geometry"] is None:
+            temp_zones = feature["properties"]["affectedZones"]
+
+            if len(temp_zones) > 1 and len(temp_zones):
+                for zone in temp_zones:
+                    zone_response = requests.get(zone).json()
+                    feature["geometry"] = zone_response["geometry"]
+            elif len(temp_zones) == 1:
+                zone_response = requests.get(temp_zones[0]).json()
+                feature["geometry"] = zone_response["geometry"]
+            else:
+                print("No Active Alerts in Oregon. QUITTING...")
+
+            temp_tbl.append({
+                "type": "Feature",
+                "geometry": feature["geometry"],
+                "properties": props
+            })
+        else:
+            temp_tbl.append({
+                "type": "Feature",
+                "geometry": feature["geometry"],
+                "properties": props
+            })
+
+    gdf = gpd.GeoDataFrame.from_features(temp_tbl, crs="EPSG:4326")
+    gdf["Description"] = gdf["Description"].str.replace("\n", "<br>")
+    print("...Complete.")
+    m = gdf.explore("Event", legend=True, cmap="plasma", tooltip=["Event", "Status", "Severity", "Headline", "Description", "Effective", "Expires"], tiles="Esri.WorldTopoMap")
+    out_file = 'noaa-alerts-full.html'
+    print(f"Saving to file: {out_file}...")
+    m.save(out_file)
+    print("...File saved.")
+    print("DONE")
